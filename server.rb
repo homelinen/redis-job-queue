9c1791dfbbf020c1c443c5ad593d70b5c061bcad
Calum Gilchrist <calum@homelinen.org>
2014-03-11 13:56:19 +0000

    Added Exception Handling in the Worker

--- server.rb
+++ server.rb
@@ -13,7 +13,14 @@ job={
 
 redis = Redis.new
 
-redis.publish('working', job.to_json)
+10.times do |i|
+    dur = (rand(i)+1).to_i
+
+    job[:arguments][:duration] = dur
+    redis.publish('working', job.to_json)
+end
+
+redis.publish('working', 'exit')
 
 puts('Done')
 
--- worker.rb
+++ worker.rb
@@ -3,18 +3,42 @@
 require 'json'
 require 'redis'
 
+require 'thread'
+
 redis = Redis.new
 
-def work(msg)
+# Translate the JSON into some method of work
+def work(msg, count)
+
+    res = true
+
     job = JSON.parse(msg)
 
     # Flatten out the task and arguments into an evaluable command
     task =[job['task'], job['arguments'].values.join].flatten.join(" ")
-    eval(task)
 
-    puts "Finished #{job['task']}"
+    begin
 
+        # Artificially create an error for job 5
+        if count == 5
+            raise ArgumentError
+        end
+
+        eval(task)
+    rescue ArgumentError
+        res = false
+    end
+
+    status = res ? 'Finished' : 'Failed'
+    
+    puts "#{count}: #{status} #{task}"
+    res
 end
+
+job_c = 0
+
+# Synchronous queue object
+job_queue = Queue.new
 
 # Subscribe and work
 redis.subscribe 'working' do |on|
@@ -22,12 +46,31 @@ redis.subscribe 'working' do |on|
     @threads = []
 
     on.message do |channel, message|
-        redis.unsubscribe if message == 'exit'
+        unless message == 'exit'
 
-        @threads << Thread.new { work(message) }
+            job_c += 1
+
+            # Create a new thread to execute the job
+            @threads << Thread.new do
+                # If a job fails, add the message into the retry queue
+                job_queue << [message, job_c] unless work(message, job_c) 
+            end
+        else
+            redis.unsubscribe 
+        end
+
+        # Retry jobs that failed
+        # Adds to queue if it fails again, should have a limit
+        until job_queue.empty?
+            job = job_queue.pop
+            puts "Trying #{job[1]} again"
+            job_c += 1
+            @threads << Thread.new { job_queue << [job[0], job_c] unless work(job[0], job_c) }
+        end
     end
 
     on.unsubscribe do |channel, sub|
+
         # Wait for all threads to complete
         @threads.each { |t| t.join }
         puts 'Unsubbed'
